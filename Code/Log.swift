a8d9f4416575d3fb098026f90ece65692ab7a3d0
Sebastian Fichtner <sebastian@nohype.ai>
2026-07-07 14:51:32 +0200

    Fix logging bug when using os_log and drop os_log entirely

--- Code/Log.swift
+++ Code/Log.swift
@@ -104,27 +104,11 @@ public extension Log
     }
 }
 
-#if canImport(os)
-import os
-#endif
-
 public actor Log
 {
     fileprivate func log(_ entry: Entry) {
         if entry.level >= minimumPrintLevel {
-            #if !canImport(os)
             print(entry.description)
-            #elseif os(macOS)
-            if #available(macOS 10.14, *) {
-                // TODO: select correct log level
-                os_log(.debug, "entry.description")
-            } else {
-                print(entry.description)
-            }
-            #else
-            // TODO: select correct log level
-            os_log(.debug, "entry.description")
-            #endif
         }
         
         receive(entry)
